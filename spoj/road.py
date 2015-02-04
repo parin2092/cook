76e6d8181ea2f2834e004a8e7641304f9245e9c5
parin2092 <parin2092@gmail.com>
2015-02-04 12:24:27 +0530

    new feature changes

--- spoj/road.py
+++ spoj/road.py
@@ -22,23 +22,6 @@ for t in xrange(tc):
 	for i in xrange(1,k+1):
 		ip[i] = map(int, raw_input().split())
 
-	# dp = [[0 for j in xrange(n+1)] for i in xrange(k+1)]
-	# dp[0][0] = 0
-
-
-	# for j in xrange(1,n+1):
-	# 	for i in xrange(1,k+1):
-	# 		if ip[i][0] < j and j <= ip[i][1]:
-	# 			dp[i][j] = min(dp[i-1][j],dp[k][ip[i][0]] + ip[i][2])
-	# 		else:
-	# 			dp[i][j] = dp[i-1][j]
-
-	# 	if not dp[k][j]:
-	# 		break
-	# if dp[k][n]:
-	# 	print dp[k][n]
-	# else:
-	# 	print -1
 	print n,k,ip
 	ddp = [111 for i in  xrange(n+1)]
 	ddp[0] = 0
